c306a0f4c8ef9af5c420d48e17738564679ac00a
Luke <lwolff@vt.edu>
2016-02-27 15:55:50 -0500

    auto-run
    auto-run script

--- background.js
+++ background.js
@@ -7,3 +7,10 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 		file: 'inject.js'
 	});
 });
+
+/*document.addEventListener('DOMContentLoaded', function(tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'inject2.js'
+	});
+});*/
--- inject2.js
+++ inject2.js
@@ -1,16 +1,20 @@
 // this is the code which will be injected into a given page...
 
+
+
+
 (function() {
 
 	// just place a div at top right
     var imgs = document.getElementsByTagName("img");
     var imgSrcs = [];
-        alert("Test before for loop");
+        //alert("Test before for loop");
     for (var i = 0; i < imgs.length; i++) {
         imgSrcs.push(imgs[i].src);
-        alert("Test in the for loop");
+		//imgs[i].src = "http://pixel.nymag.com/imgs/daily/intelligencer/2015/08/10/10-donald-trump-debate.w750.h560.2x.jpg";
+        //alert("Test in the for loop");
     }
-alert(JSON.stringify(imgSrcs));
-alert(imgSrcs.join("\n"));
+//alert(JSON.stringify(imgSrcs));
+//alert(imgSrcs.join("\n"));
 
 })();
